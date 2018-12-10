b840adb70a48644a7e64ea44dcd6ffbef3d121c2
douniwan <douniwan@github.com>
2018-12-10 18:04:44 +0800

    step 6

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,23 @@
 var cacheName = 'weatherPWA-step-6-1';
-var filesToCache = [];
+var filesToCache = [
+  '/pwa/',
+  '/pwa/index.html',
+  '/pwa/scripts/app.js',
+  '/pwa/styles/inline.css',
+  '/pwa/images/clear.png',
+  '/pwa/images/cloudy-scattered-showers.png',
+  '/pwa/images/cloudy.png',
+  '/pwa/images/fog.png',
+  '/pwa/images/ic_add_white_24px.svg',
+  '/pwa/images/ic_refresh_white_24px.svg',
+  '/pwa/images/partly-cloudy.png',
+  '/pwa/images/rain.png',
+  '/pwa/images/scattered-showers.png',
+  '/pwa/images/sleet.png',
+  '/pwa/images/snow.png',
+  '/pwa/images/thunderstorm.png',
+  '/pwa/images/wind.png'
+];;
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
@@ -7,6 +25,28 @@ self.addEventListener('install', function(e) {
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
